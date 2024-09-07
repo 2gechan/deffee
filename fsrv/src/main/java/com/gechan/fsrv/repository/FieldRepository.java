10f7e4cdbf271666408cd810001785ae582eda4c
7532602 <7532602@gmail.com>
2024-09-07 16:53:43 +0900

    rm -rf fsrv

--- fsrv/src/main/java/com/gechan/fsrv/repository/FieldRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gechan.fsrv.repository;
-
-import com.gechan.fsrv.domain.field.Field;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FieldRepository extends JpaRepository<Field, Long> {
-
-}
--- fsrv/src/main/java/com/gechan/fsrv/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gechan.fsrv.repository;
-
-import com.gechan.fsrv.domain.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
